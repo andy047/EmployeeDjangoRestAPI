b5cbe62ad0b108c426bbe6e9417174f38478ea5e
andyr <andyrodriguezjimenez@gmail.com>
2021-11-20 21:58:23 -0400

    funcionando con path en urls de apps

--- DjangoRESTAPI/Employee/urls.py
+++ DjangoRESTAPI/Employee/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.urls.conf import path
 
 from Employee import views
 
 urlpatterns = [
-    url(r'^department$',views.departmentApi),
-    url(r'^department/([0-9]+)'),
+    #url(r'^department$',views.departmentApi),
+    #url(r'^department/([0-9]+)'),
+    path('department/<int:id>',views.departmentApi)
 
 ]
